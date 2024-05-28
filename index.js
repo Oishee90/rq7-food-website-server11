a36d0d6c9a878ceee4cdb34fb37a10121d8c542e
oishee <oisheekhan90@gmail.com>
2024-05-29 01:52:56 +0600

    food post complete

--- index.js
+++ index.js
@@ -25,14 +25,32 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+  
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const foodsCollection = client.db('foodDB').collection('food');
+    // console.log(foodCollection)
+    app.get("/addFood", async(req,res) => {
+        const cursor = foodsCollection.find();
+        const result = await cursor.toArray()
+        res.send(result)
+        
+    })
+    app.post("/addFood", async (req, res) => {
+        const newFood = req.body;
+        console.log(newFood);
+        const result = await foodsCollection.insertOne(newFood)
+        res.send(result)
+        // Here you can add your logic to save the new food item to the database
+      });
+
+ 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    // await client.close();
+        
   }
 }
 run().catch(console.dir);
